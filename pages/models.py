7969b810f8d2ab64e7be40af6e639eb557468505
Harry Moreno <morenoh149@gmail.com>
2020-04-19 17:19:57 -0400

    Add minimal example of map with events from db

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    """Model representing an event."""
+    title = models.CharField(
+        max_length=200,
+        help_text="Enter the title of your event."
+    )
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String representation of the Event."""
+        return f"{self.title} - {self.latitude}, {self.longitude}"
